fd44b99c3c55d00e32466566120814e1dd916961
Tew Tawan <ttawanc@gmail.com>
2025-03-31 22:17:42 +1100

    feat(employee): update interface

--- libs/types/index.ts
+++ libs/types/index.ts
@@ -3,11 +3,11 @@ export type EmployeeType = 'FULL_TIME' | 'PART_TIME' | 'CONTRACTOR';
 export type Employee = {
     uuid: string,
     full_name: string,
-    phone_number: string,
+    phone_number?: string,
     email_address: string,
-    biography: string,
-    photo_url_small: string,
-    photo_url_large: string,
+    biography?: string,
+    photo_url_small?: string,
+    photo_url_large?: string,
     team?: string,
     employee_type: EmployeeType,
 };
